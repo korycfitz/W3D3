21d7b167e8617e85b8d494ed148bfca612debfb5
Bennett <drbmgit@gmail.com>
2022-09-28 16:41:47 -0400

    deepdup

--- recursion_exercises.rb
+++ recursion_exercises.rb
@@ -16,7 +16,7 @@ end
 # p recursive_sum(0, 1)
 
 def iterative_sum(num_1, num_2)
-    return [num_1] if num_1 == num_2 
+    return [num_1] if num_1 == num_2
     arr = []
     (num_1...num_2).each { |el| arr << el }
     arr
@@ -26,13 +26,17 @@ end
 # p recursive_sum(1, 1)
 # p recursive_sum(2, 1)
 # p recursive_sum(0, 1)
+def exp1(b,n)
+    return 1 if n == 0
+    b * exp1(b, n-1)
+end
 
 def exp(b, n)
     #even n only
-    return 1 if n == 0 
+    return 1 if n == 0
     return b if n == 1
     return 0 if b == 0 && n == 1 ## return val
-    if n.even? 
+    if n.even?
         exp(b, n/2) * exp(b, n/2)
     else
         b * exp(b, (n-1)/2) * exp(b, (n-1)/2)
@@ -40,3 +44,21 @@ def exp(b, n)
 end
 
 # p exp(10, 20)
+
+def deep_dup(array)
+
+    return array if !array.is_a? Array
+    new_arr = []
+    array.each {|subarray| new_arr << deep_dup(subarray)}
+
+    new_arr
+
+end
+
+
+array = [
+  ["nuts", "bolts", "washers"],
+  ["capacitors", "resistors", "inductors"]
+]
+
+p deep_dup(array)
