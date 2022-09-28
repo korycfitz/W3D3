67b5a2a6566834f4dc128eb57ecdf221f79caaac
korycfitz <korycfitz@gmail.com>
2022-09-28 15:20:39 -0400

    exponential problem finished

--- recursion_exercises.rb
+++ recursion_exercises.rb
@@ -14,3 +14,29 @@ end
 # p recursive_sum(1, 1)
 # p recursive_sum(2, 1)
 # p recursive_sum(0, 1)
+
+def iterative_sum(num_1, num_2)
+    return [num_1] if num_1 == num_2 
+    arr = []
+    (num_1...num_2).each { |el| arr << el }
+    arr
+end
+
+# p recursive_sum(1, 5)
+# p recursive_sum(1, 1)
+# p recursive_sum(2, 1)
+# p recursive_sum(0, 1)
+
+def exp(b, n)
+    #even n only
+    return 1 if n == 0 
+    return b if n == 1
+    return 0 if b == 0 && n == 1 ## return val
+    if n.even? 
+        exp(b, n/2) * exp(b, n/2)
+    else
+        b * exp(b, (n-1)/2) * exp(b, (n-1)/2)
+    end
+end
+
+# p exp(10, 20)
